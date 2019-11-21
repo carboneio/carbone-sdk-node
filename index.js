6a6d4e7025c5ae71b21c16298a5d2b9106284b27
Maxime Magné <mmagne@easilys.com>
2019-11-21 14:15:54 +0100

    Add get/add/del template;

--- index.js
+++ index.js
@@ -1,33 +1,211 @@
 const request = require('request');
 const fs = require('fs')
 const path = require('path')
+const Transform = require('stream').Transform
+const StreamAnswer = require('./streamAnswer')
 
-module.exports = (apiKey) => {
-  return {
-    addTemplate: (localPath, payload, callback) => {
-      if (!checkPathIsAbsolute(localPath)) {
-        return callback(new Error('You path must be an absolute path'));
-      }
+let _apiKey = '';
+let _config = {
+  carboneUrl: 'https://render.carbone.io/'
+};
 
-      request({
-        method: 'POST',
-        uri: 'http://localhost:3000/template',
-        formData: {
-          payload: payload,
-          template: fs.createReadStream(localPath)
-        },
-        headers: {
-          authorization: `Bearer ${apiKey}`
-        }
-      }, (err, res, body) => {
-        if (err) {
-          return callback(err);
-        }
-
-        return callback(null, body);
-      });
+function parseResponse (response, body, wantedKey, callback) {
+  try {
+    body = JSON.parse(body);
+  } catch (e) {
+    // Return JSON parse error only if the request is expected to work well
+    if (response.statusCode === 200) {
+      console.error('Body parsed ', body);
+      return callback(new Error('Cannot parse body'));
     }
   }
+
+  if (response.statusCode !== 200) {
+    if (body && body.success === false) {
+      return callback(new Error(body.error));
+    }
+    return callback(new Error(response.statusMessage));
+  }
+
+  if (body.success === false) {
+    return callback(new Error(body.error));
+  }
+
+  if (wantedKey !== undefined) {
+    return callback(null, body.data[wantedKey]);
+  }
+
+  return callback(null);
+}
+
+function parseStreamedResponse (response, callback) {
+  if (response.statusCode === 404) {
+    return callback(new Error('File not found'));
+  } else if (response.statusCode !== 200) {
+    return callback(new Error(`Error ${response.statusCode}: an error occured`));
+  }
+
+  return callback(null);
+}
+
+function returnStreamOrCallbackError (error, response, callback) {
+  if (callback) {
+    return callback(error);
+  }
+
+  return response.emit('error', error);
+}
+
+const sdkFunctions = {
+  config: function (userConfig) {
+    config = Object.assign(config, userConfig)
+  },
+
+  /**
+   * Upload user template
+   * @param {String} localPath User filepath
+   * @param {String} payload User payload
+   * @param {Function} callback
+   * @param {Boolean} _retry False if the request has not been retry
+   */
+  addTemplate: function (localPath, payload, callback, _retry = false) {
+    if (callback === undefined) {
+      callback = payload;
+      payload = '';
+    }
+
+    if (!checkPathIsAbsolute(localPath)) {
+      return callback(new Error('Your path must be an absolute path'));
+    }
+
+    request({
+      method: 'POST',
+      uri: `${_config.carboneUrl}template`,
+      formData: {
+        payload: payload,
+        template: fs.createReadStream(localPath)
+      },
+      headers: {
+        authorization: `Bearer ${_apiKey}`
+      }
+    }, (err, response, body) => {
+      if (err) {
+        if (err.code === 'ECONNRESET' && !_retry) {
+          return this.addTemplate(localPath, payload, callback, true);
+        }
+
+        return callback(err);
+      }
+
+      return parseResponse(response, body, 'templateId', callback);
+    });
+  },
+
+  /**
+   * Delete a template with the templateID
+   * @param {String} templateId Template ID to delete
+   * @param {Function} callback
+   * @param {Boolean} _retry False if the request has not been retry
+   */
+  delTemplate: function (templateId, callback, _retry = false) {
+    if (templateId == null) {
+      return callback(new Error('Invalid template ID'));
+    }
+
+    request({
+      method: 'DELETE',
+      uri: `${_config.carboneUrl}template/${templateId}`,
+      headers: {
+        authorization: `Bearer ${_apiKey}`
+      }
+    }, (err, response, body) => {
+      if (err) {
+        if (err.code === 'ECONNRESET' && !_retry) {
+          return this.delTemplate(templateId, callback, true);
+        }
+
+        return callback(err);
+      }
+
+      return parseResponse(response, body, undefined, callback);
+    });
+  },
+
+  /**
+   * Return the content of the file if a callback is provided. Else it returns a stream with the file
+   * @param {String} templateId Template Id to retrieve
+   * @param {Function} callback
+   */
+  getTemplate: function (templateId, callback) {
+    if (templateId == null) {
+      return callback(new Error('Invalid template ID'));
+    }
+
+    // Create the stream before starting the request
+    // So we can return it to the user
+    let stream = StreamAnswer();
+
+    request({
+      method: 'GET',
+      uri: `${_config.carboneUrl}template/${templateId}`,
+      headers: {
+        authorization: `Bearer ${_apiKey}`
+      }
+    }).on('response', (response) => {
+      parseStreamedResponse(response, (err) => {
+        if (err) {
+          return returnStreamOrCallbackError(err, stream, callback);
+        }
+
+        stream.setHeaders(response.headers);
+
+        if (callback === undefined) {
+          return response.pipe(stream);
+        }
+
+        let content = '';
+
+        response.on('data', (chunk) => {
+          content += chunk;
+        });
+
+        response.on('end', () => {
+          return callback(null, content);
+        });
+      });
+    }).on('error', (err) => {
+      return returnStreamOrCallbackError(err, stream, callback);
+    });
+
+    return stream;
+  },
+
+  /**
+   * Return the filename located in the headers response
+   * @param {Object} streamedResponse Request response object
+   */
+  getTemplateFilename: function (streamedResponse) {
+    if (streamedResponse.headers == null || streamedResponse.headers['content-disposition'] == null) {
+      return null;
+    }
+
+    let contentDisposition = streamedResponse.headers['content-disposition'];
+    let splitted = contentDisposition.split('=')
+
+    if (splitted.length === 1) {
+      return null;
+    }
+
+    let filename = splitted[1]
+    filename = filename.substr(0, filename.length - 1);
+    filename = filename.substr(1);
+    return filename;
+  }
+};
+
+module.exports = (apiKey) => {
+  _apiKey = apiKey
+  return sdkFunctions;
 }
 
 /**
--- test/sdk.js
+++ test/sdk.js
@@ -1,14 +1,459 @@
 const assert = require('assert');
 const path = require('path');
-const sdk = require('../index')('eyJhbGciOiJFUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1NSIsImF1ZCI6ImNhcmJvbmUiLCJleHAiOjIyMDQ0NjkwMjcsImRhdGEiOnsiaWRBY2NvdW50Ijo1NX19.AC4vwhPF3FBBmxywXnpJNvapUssq3N9D7FpYJFMe8brp-Sm_ZMVpccCk0cEXhOIn59lnAzwCsUzGkWmzT0cFduQ6ALtPOoQzcSE-eBdA30EdgnGmQnT7LkrQPCy62wjdRXaT3JZUigUxLEcCZL8E4UHoIxUc4WW10X5Y-DJFLlS-ilNi');
+const sdk = require('../index')('CARBONE_API_KEY');
+const nock = require('nock');
+const fs = require('fs');
+
+const CARBONE_URL = 'https://render.carbone.io/'
 
 describe('Carbone SDK', () => {
   describe('Add template', () => {
     it('should add a template', (done) => {
-      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), 'toto', (err, body) => {
-        console.log(err, body)
-        done()
+      nock(CARBONE_URL)
+        .post((uri) => uri.includes('template'))
+        .reply(200, {
+          success: true,
+          data: {
+            templateId: 'fileTemplateId'
+          }
+        });
+
+      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), 'toto', (err, templateId) => {
+        assert.strictEqual(err, null);
+        assert.strictEqual(templateId, 'fileTemplateId');
+        done();
+      });
+    });
+
+    it('should add a template without payload', (done) => {
+      nock(CARBONE_URL)
+        .post((uri) => uri.includes('template'))
+        .reply(200, {
+          success: true,
+          data: {
+            templateId: 'newTemplateId'
+          }
+        });
+
+      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), (err, templateId) => {
+        assert.strictEqual(err, null);
+        assert.strictEqual(templateId, 'newTemplateId');
+        done();
+      });
+    });
+
+    it('should return an error if filepath is not absolute', (done) => {
+      sdk.addTemplate('./relative', (err, templateId) => {
+        assert.strictEqual(err.message, 'Your path must be an absolute path');
+        done();
       })
+    });
+
+    it('should return an error if carbone return success = false', (done) => {
+      nock(CARBONE_URL)
+        .post((uri) => uri.includes('template'))
+        .reply(200, {
+          success: false,
+          error: 'An error occured with the API'
+        });
+
+      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), 'toto', (err, templateId) => {
+        assert.strictEqual(err.message, 'An error occured with the API');
+        done();
+      });
+    });
+
+    it('should return an error from body if carbone return a statusCode different than 200', (done) => {
+      nock(CARBONE_URL)
+        .post((uri) => uri.includes('template'))
+        .reply(400, {
+          success: false,
+          error: '400 server error'
+        });
+
+      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), 'toto', (err, templateId) => {
+        assert.strictEqual(err.message, '400 server error');
+        done();
+      });
+    });
+
+    it('should return an error with status message if body is undefined', (done) => {
+      nock(CARBONE_URL)
+        .post((uri) => uri.includes('template'))
+        .reply(300);
+
+      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), 'toto', (err, templateId) => {
+        // The status message is null because it cannot be set with nock
+        assert.strictEqual(err.message, 'null');
+        done();
+      });
+    });
+
+    it('should return a request error', (done) => {
+      nock(CARBONE_URL)
+        .post((uri) => uri.includes('template'))
+        .replyWithError('REQUEST ERROR');
+
+      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), 'toto', (err, templateId) => {
+        assert.strictEqual(err.message, 'REQUEST ERROR');
+        done();
+      });
+    });
+
+    it('should retry the request if the request error is an ECONNRESET', (done) => {
+      let mockRequest = nock(CARBONE_URL)
+        .post((uri) => uri.includes('template'))
+        .replyWithError({ code: 'ECONNRESET' })
+        .post((uri) => uri.includes('template'))
+        .reply(200, {
+          success: true,
+          data: {
+            templateId: 'fileTemplateId'
+          }
+        });
+
+      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), 'toto', (err, templateId) => {
+        assert.strictEqual(err, null);
+        assert.strictEqual(templateId, 'fileTemplateId');
+        assert.strictEqual(mockRequest.pendingMocks().length, 0);
+        done();
+      });
+    });
+
+    it('should retry the request only one time for a ECONNRESET error', (done) => {
+      let mockRequest = nock(CARBONE_URL)
+        .post((uri) => uri.includes('template'))
+        .replyWithError({ code: 'ECONNRESET' })
+        .post((uri) => uri.includes('template'))
+        .replyWithError({ code: 'ECONNRESET', message: 'Connection reset' });
+
+      sdk.addTemplate(path.join(__dirname, 'datasets', 'test.odt'), 'toto', (err, templateId) => {
+        assert.strictEqual(err.message, 'Connection reset');
+        assert.strictEqual(mockRequest.pendingMocks().length, 0);
+        done();
+      });
+    });
+  });
+
+  describe('Delete template', () => {
+    it('should delete a template', (done) => {
+      nock(CARBONE_URL)
+        .delete((uri) => uri.includes('template'))
+        .reply(200, {
+          success: true,
+          error: null
+        });
+
+      sdk.delTemplate('templateId', (err) => {
+        assert.strictEqual(err, null);
+        done();
+      });
+    });
+
+    it('should return an error if templateId is null', (done) => {
+      sdk.delTemplate(null, (err) => {
+        assert.strictEqual(err.message, 'Invalid template ID');
+        done();
+      });
+    });
+
+    it('should return an error if templateId is undefined', (done) => {
+      sdk.delTemplate(undefined, (err) => {
+        assert.strictEqual(err.message, 'Invalid template ID');
+        done();
+      });
+    });
+
+    it('should return error from body if success = false', (done) => {
+      nock(CARBONE_URL)
+        .delete((uri) => uri.includes('template'))
+        .reply(200, {
+          success: false,
+          error: 'Error with your template ID'
+        });
+
+      sdk.delTemplate('templateId', (err) => {
+        assert.strictEqual(err.message, 'Error with your template ID');
+        done();
+      });
+    });
+
+    it('should return statusMessage error if code is not 200', (done) => {
+      nock(CARBONE_URL)
+        .delete((uri) => uri.includes('template'))
+        .reply(400);
+
+      sdk.delTemplate('templateId', (err) => {
+        // The status message is null because it cannot be set with nock
+        assert.strictEqual(err.message, 'null');
+        done();
+      });
+    });
+
+    it('should return error from body if status is not 200 and body is defined', (done) => {
+      nock(CARBONE_URL)
+        .delete((uri) => uri.includes('template'))
+        .reply(400, {
+          success: false,
+          error: 'Bad template ID'
+        });
+
+      sdk.delTemplate('templateId', (err) => {
+        assert.strictEqual(err.message, 'Bad template ID');
+        done();
+      });
+    });
+
+    it('should return error if the request fails', (done) => {
+      nock(CARBONE_URL)
+        .delete((uri) => uri.includes('template'))
+        .replyWithError('Request error');
+
+      sdk.delTemplate('templateId', (err) => {
+        assert.strictEqual(err.message, 'Request error');
+        done();
+      });
+    });
+
+    it('should retry the request if the first reponse is an ECONNRESET error', (done) => {
+      nock(CARBONE_URL)
+        .delete((uri) => uri.includes('template'))
+        .replyWithError({ code: 'ECONNRESET' })
+        .delete((uri) => uri.includes('template'))
+        .reply(200, {
+          success: true,
+          error: null
+        });
+
+      sdk.delTemplate('templateId', (err) => {
+        assert.strictEqual(err, null);
+        done();
+      });
+    });
+
+    it('should retry the request only one time before returning the ECONNREST error', (done) => {
+      nock(CARBONE_URL)
+        .delete((uri) => uri.includes('template'))
+        .replyWithError({ code: 'ECONNRESET', message: 'ECONNRESET error' })
+        .delete((uri) => uri.includes('template'))
+        .replyWithError({ code: 'ECONNRESET', message: 'ECONNRESET error' });
+
+      sdk.delTemplate('templateId', (err) => {
+        assert.strictEqual(err.message, 'ECONNRESET error');
+        done();
+      });
+    });
+  });
+
+  describe('Get template', () => {
+    describe('Callback', () => {
+      it('should return the content of the file in the callback', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .reply(200, (uri, requestBody) => {
+            return fs.createReadStream(path.join(__dirname, 'datasets', 'streamedFile.txt'))
+          });
+
+        sdk.getTemplate('templateId', (err, content) => {
+          assert.strictEqual(err, null);
+          assert.strictEqual(content, 'Hello I am the streamed file!\n');
+          done();
+        });
+      });
+
+      it('should return a file not found error if code is 404', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .reply(404);
+
+        sdk.getTemplate('templateId', (err, content) => {
+          assert.strictEqual(err.message, 'File not found');
+          done();
+        });
+      });
+
+      it('should return an error with the code if the code is different than 200', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .reply(303);
+
+        sdk.getTemplate('templateId', (err, content) => {
+          assert.strictEqual(err.message, 'Error 303: an error occured');
+          done();
+        });
+      });
+
+      it('should return an error if the request failed', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .replyWithError('Request error');
+
+        sdk.getTemplate('templateId', (err, content) => {
+          assert.strictEqual(err.message, 'Request error');
+          done();
+        });
+      });
+
+      it('should return an error if the template ID is null', (done) => {
+        sdk.getTemplate(null, (err, content) => {
+          assert.strictEqual(err.message, 'Invalid template ID');
+          done();
+        });
+      });
+
+      it('should return an error if the template ID is undefined', (done) => {
+        sdk.getTemplate(undefined, (err, content) => {
+          assert.strictEqual(err.message, 'Invalid template ID');
+          done();
+        });
+      });
+    });
+
+    describe('Get template filename', () => {
+      it('should return the filename in headers', () => {
+        let obj = {
+          headers: {
+            'content-disposition': 'filename="tata.txt"'
+          }
+        };
+
+        let filename = sdk.getTemplateFilename(obj);
+        assert.strictEqual(filename, 'tata.txt');
+      });
+
+      it('should return null if object has no headers key', () => {
+        let obj = {
+        };
+
+        let filename = sdk.getTemplateFilename(obj);
+        assert.strictEqual(filename, null);
+      });
+
+      it('should return null if headers has no content-disposition key', () => {
+        let obj = {
+          headers: {}
+        };
+
+        let filename = sdk.getTemplateFilename(obj);
+        assert.strictEqual(filename, null);
+      });
+
+      it('should return null if content-disposition is empty', () => {
+        let obj = {
+          headers: {
+            'content-disposition': ''
+          }
+        };
+
+        let filename = sdk.getTemplateFilename(obj);
+        assert.strictEqual(filename, null);
+      });
+    });
+
+    describe('Stream', () => {
+      let _filename = null;
+
+      after(() => {
+        if (_filename !== null) {
+          fs.unlinkSync(path.join(__dirname, _filename));
+        }
+      });
+
+      it('should return the content of the file with a stream and write the new file', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .reply(200, (uri, requestBody) => {
+            return fs.createReadStream(path.join(__dirname, 'datasets', 'streamedFile.txt'))
+          }, {
+            'Content-Disposition': 'filename="tata.txt"'
+          });
+
+        let filename = 'test.txt';
+        let writeStream = fs.createWriteStream(path.join(__dirname, filename));
+
+        writeStream.on('close', () => {
+          let newContentFile = fs.readFileSync(path.join(__dirname, 'test.txt'), 'utf8');
+          assert.strictEqual(newContentFile, 'Hello I am the streamed file!\n');
+          _filename = 'test.txt';
+          done();
+        });
+
+        sdk.getTemplate('templateId').pipe(writeStream);
+      });
+
+      it('should return an error if the file does not exists', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .reply(404);
+
+        let filename = 'test.txt';
+        let writeStream = fs.createWriteStream(path.join(__dirname, filename));
+        let fileStream = sdk.getTemplate('templateId');
+
+        fileStream.on('error', (err) => {
+          assert.strictEqual(err.message, 'File not found');
+          done();
+        });
+
+        fileStream.pipe(writeStream);
+      });
+
+      it('should return an error if carbone return another code than 200', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .reply(300);
+
+        let filename = 'test.txt';
+        let writeStream = fs.createWriteStream(path.join(__dirname, filename));
+        let fileStream = sdk.getTemplate('templateId');
+
+        fileStream.on('error', (err) => {
+          assert.strictEqual(err.message, 'Error 300: an error occured');
+          done();
+        });
+
+        fileStream.pipe(writeStream);
+      });
+
+      it('should return an error if the requets fails', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .replyWithError('Request error');
+
+        let filename = 'test.txt';
+        let writeStream = fs.createWriteStream(path.join(__dirname, filename));
+        let fileStream = sdk.getTemplate('templateId');
+
+        fileStream.on('error', (err) => {
+          assert.strictEqual(err.message, 'Request error');
+          done();
+        });
+
+        fileStream.pipe(writeStream);
+      });
+
+      it('should return the filename is headers', (done) => {
+        nock(CARBONE_URL)
+          .get((uri) => uri.includes('template'))
+          .reply(200, (uri, requestBody) => {
+            return fs.createReadStream(path.join(__dirname, 'datasets', 'streamedFile.txt'))
+          }, {
+            'Content-Disposition': 'filename="tata.txt"'
+          });
+
+        let fileStream = sdk.getTemplate('templateId');
+        let filename = 'test.txt';
+        let writeStream = fs.createWriteStream(path.join(__dirname, filename));
+
+        writeStream.on('close', () => {
+          let filename = sdk.getTemplateFilename(fileStream)
+          assert.strictEqual(filename, 'tata.txt');
+          _filename = 'test.txt';
+          done();
+        });
+
+        fileStream.pipe(writeStream);
+      });
     });
   });
 });
